1f28770335147e6847e329ea7a9c9bb26609b1fc
frogefan <frogefroge@gmx.de>
2024-03-23 11:30:29 +0100

    style changes

---
+++ src/app/content/common/custom-title-case.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core'
+import { TitleCasePipe as AngularTitleCasePipe } from '@angular/common'
+
+@Pipe({
+  name: 'customTitleCase'
+})
+export class CustomTitleCasePipe implements PipeTransform {
+  angularTitleCase = new AngularTitleCasePipe()
+  transform (value: any, ...args: any[]): any {
+    const property = args[0]
+    value = this.angularTitleCase.transform(value)
+    value = value.replaceAll('Frogex', 'FrogeX').replaceAll('Erc', 'ERC')
+    return value
+  }
+}
